ac1f750d052a9383a58f24865e8e93be06ed5553
Evan Langer <elanger@live.com>
2023-12-07 19:40:54 -0600

    made feelingInput reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,19 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 
 
+const feelingInput = (state = "", action) => {
+    if(action.type === "SAVE_FEELING_INPUT") {
+        console.log(action.payload, "feelingResult")
+        return(
+            state + action.payload
+        )
+    } return state;
+}
+
+
 const store = createStore(
     combineReducers({
+        feelingInput,
 
     }),
     applyMiddleware(logger),
